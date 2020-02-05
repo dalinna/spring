9dc55ee869e02f9351549278163ee3495a9dd98d
tanlinlin <805148199@xiaozhu.com>
2020-02-05 22:59:46 +0800

    fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,7 +80,7 @@ export default {
   methods: {
     getData() {
       const api = 'http://127.0.0.1:8000/getInfo';
-      this.$http.post(api).then((response) => {
+      this.$http.get(api).then((response) => {
         this.formData = JSON.parse(response.data).data;
       });
     },
